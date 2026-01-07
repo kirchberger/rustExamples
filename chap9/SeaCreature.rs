aa3287ab431b122f1fb11688cd0e84826b741b4e
kirchberger <kirchbergeralex0@gmail.com>
2026-01-07 15:21:07 -0500

    Finished

---
+++ chap9/SeaCreature.rs
@@ -0,0 +1,8 @@
+pub struct SeaCreature {
+  pub animal_type: String,
+  pub name: String,
+  pub arms: i32,
+  pub legs: i32,
+
+  weapon: String,
+}
---
+++ chap9/refmodandcrate.rs
@@ -0,0 +1,5 @@
+use std::f64::consts::PI;
+
+fn main(){
+  println!("Welcome to the playground!\nI would like a slice of {}!", PI);
+}
